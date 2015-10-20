264c9895aebd67864007d618c3a6e78daf250d42
Jerzy Spendel <jspendel@gmail.com>
2015-10-20 21:47:22 +0200

    Implemented LoadBalancer

---
+++ redscraper/balancer.py
@@ -0,0 +1,64 @@
+import asyncio
+import fractions
+import time
+import math
+
+
+class LoadBalancer:
+    MINUTE = 60
+    SECOND = 1
+
+    def __init__(self):
+        self.limit = 60
+        self.fullness_rate = fractions.Fraction(0, self.limit)
+        self.unit_type = LoadBalancer.MINUTE
+        self.t = time.time()
+
+    @property
+    def dt(self):
+        return time.time() - self.t
+
+    def _update_fullness_rate(self):
+        '''
+        `free` - how much of fullness_rate is going to be freed
+        `consumed_time` - since last time we called `_update_fullness_rate`
+        some time passed and it is `dt`, but not all `dt` have been consumed to
+        lower `fullness_rate` so the rest we put to self.t
+        '''
+        free = self.dt*self.limit/self.unit_type
+        if free >= 1:
+            self.fullness_rate -= fractions.Fraction(math.floor(free), self.limit)
+            if self.fullness_rate < 0:
+                self.fullness_rate = fractions.Fraction(0, self.limit)
+            consumed_time = math.floor(free)*self.unit_type/self.limit
+            self.t += consumed_time #dt is now lower
+
+    def _increment(self):
+        self.fullness_rate += fractions.Fraction(1, self.limit)
+
+    def set_requests_limit(self, limit):
+        '''
+        :param limit: max requests per unit of time
+        :return:
+        '''
+        self.limit = limit
+
+    def get_requests_limit(self):
+        '''
+        :return: max requests per unit of time
+        '''
+        return self.limit
+
+    @asyncio.coroutine
+    def ask(self):
+        '''
+        Ask for permission to continue code execution
+        after yielding from this coroutine
+        '''
+        self._update_fullness_rate()
+        if self.fullness_rate < 1:
+            self._increment()
+            return
+        else:
+            yield from asyncio.sleep(0.5) #TODO: delete hardcoded time
+            yield from self.ask()
--- redscraper/tests.py
+++ redscraper/tests.py
@@ -1,11 +1,13 @@
 import unittest
-import aioredis
 import asyncio
 from .scraper import RedisURLDispatcher
 from .scraper import URLDispatcher
 from .scraper import CrawlersManager
 from .helpers import normalize_url
 from .helpers import is_relative
+from .balancer import LoadBalancer
+import random
+import time
 
 
 class RedisURLDispatcherTestCase(unittest.TestCase):
@@ -63,3 +65,55 @@ class CrawlersManagerTestCase(unittest.TestCase):
 
     def tearDown(self):
         self.cm._close_connections()
+
+
+class LoadBalancerConfigurationTestCase(unittest.TestCase):
+    def setUp(self):
+        super().setUp()
+        self.balancer = LoadBalancer()
+
+    def test_set_max_requests_per_minute(self):
+        self.balancer.set_requests_limit(10)
+        self.assertEqual(10, self.balancer.get_requests_limit())
+
+
+class LoadBalancerTestCase(unittest.TestCase):
+    def setUp(self):
+        super().setUp()
+        self.loop = asyncio.get_event_loop()
+        self.balancer = LoadBalancer()
+        self.balancer.set_requests_limit(60)
+
+    def test_simple_asking(self):
+
+        @asyncio.coroutine
+        def tester(future):
+            t = time.time()
+            yield from self.balancer.ask()
+            future.set_result(time.time() - t)
+
+        future = asyncio.Future()
+        self.loop.run_until_complete(tester(future))
+        self.assertLess(future.result(), 0.1)
+
+    def test_balancer_time_limit(self):
+        request_time = 0.1
+
+        @asyncio.coroutine
+        def request_faker():
+            yield from self.balancer.ask()
+            yield from asyncio.sleep(request_time)
+
+        @asyncio.coroutine
+        def testing_coroutine(future):
+            coro_list = []
+            t = time.time()
+            for i in range(61):
+                task = asyncio.Task(request_faker())
+                coro_list.append(task)
+            yield from asyncio.wait(coro_list)
+            future.set_result(time.time() - t)
+
+        future = asyncio.Future()
+        self.loop.run_until_complete(testing_coroutine(future))
+        self.assertGreater(future.result(), request_time)
