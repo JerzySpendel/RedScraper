a5134293f77b0fbcf775e9ee4bafd36d63409657
Jerzy Spendel <jspendel@gmail.com>
2015-11-17 12:20:22 +0000

    New projects files are not created if already exists

--- redscraper/redscraper.py
+++ redscraper/redscraper.py
@@ -11,11 +11,20 @@ parsed = parser.parse_args()
 package = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 schema_path = os.path.join(package, 'redscraper', 'config_schema.yml')
 sample_path = os.path.join(package, 'redscraper', 'sample_new_project.py.sample')
+write_schema_path = os.path.join(parsed.name, parsed.name+'.py')
+write_config_path = os.path.join(parsed.name, 'config.yml')
 
 if not os.path.isdir(parsed.name):
     os.mkdir(parsed.name)
 else:
     print('New project is going to be deployed in existing directory')
 
-open(os.path.join(parsed.name, parsed.name+'.py'), 'w').write(open(sample_path, 'r').read())
-open(os.path.join(parsed.name, 'config.yml'), 'w').write(open(schema_path, 'r').read())
+if not os.path.exists(write_schema_path):
+    open(write_schema_path, 'w').write(open(sample_path, 'r').read())
+else:
+    print('File {} already exists'.format(os.path.split(write_schema_path)[1]))
+
+if not os.path.exists(write_config_path):
+    open(write_config_path, 'w').write(open(schema_path, 'r').read())
+else:
+    print('File {} already exists'.format(os.path.split(write_config_path)[1]))
