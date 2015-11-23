99d8718a47d8be39b03d56d16ed1a2807456adda
Jerzy Spendel <jspendel@gmail.com>
2015-11-23 09:47:29 +0000

    Added simple readme and modified setup.py to install requirements

---
+++ README.md
@@ -0,0 +1,3 @@
+Redscraper is a simple library to write scrapping data crawlers.
+
+You can try this running example.py or by creating new project by redscraper.py -n project_name
--- setup.py
+++ setup.py
@@ -4,11 +4,18 @@ from setuptools import setup
 setup(
     name='redscraper',
     version='0.1',
-    author='Jerzy Spendel',
+    author='SkyGate',
     packages=['redscraper'],
     scripts=['redscraper/redscraper.py'],
     package_data={'redscraper': ['config_schema.yml',
                                  'sample_new_project.py.sample'
                                  ]},
     include_package_data=True,
+    install_requires=[
+        'aioredis',
+        'aiohttp',
+        'beautifulsoup4',
+        'pyyaml',
+        'lxml',
+    ]
 )
