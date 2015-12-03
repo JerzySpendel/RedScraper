6534a49d49812f67d6b102f0c93ef5603320f279
Jerzy Spendel <jspendel@gmail.com>
2015-12-03 12:19:51 +0000

    [#4] Added basic tests for CrawlersManager

--- redscraper/tests/test_crawlers_manager.py
+++ redscraper/tests/test_crawlers_manager.py
@@ -1,7 +1,7 @@
 import asyncio
 from .utils import HttpServer
 from .utils import TestingProcessor
-from redscraper.scraper import CrawlersManager
+from redscraper.scraper import CrawlersManager, Crawler
 from redscraper.settings import config
 import pytest
 
@@ -19,13 +19,47 @@ class TestManagerCrawlers:
 
     def test_run_and_stop(self):
 
-        @asyncio.coroutine
-        def cm():
-            yield from self.cm.run()
-            yield from self.cm.stop()
-
-        self.loop.run_until_complete(cm())
+        self.loop.run_until_complete(self.cm.run())
+        assert self.cm.state == 'running'
+        self.loop.run_until_complete(self.cm.stop())
         assert self.cm.state == 'stopped'
+        assert self.cm.concurrent == 0
+        assert len(self.cm.crawlers) == 0
+
+    def test__new_crawler(self):
+        c = self.cm._new_crawler()
+        assert isinstance(c, Crawler)
+
+    def test_fire_one(self):
+        c = self.cm.fire_one()
+        assert len(c.future._callbacks) == 1
+
+    def test_setting_url_constraint(self):
+        c = lambda: True
+        self.cm.set_url_constraint(c)
+        assert self.cm.url_constraints == [c]
+
+    def test_adding_url_constraints(self):
+        c = lambda: True
+        d = lambda: False
+        self.cm.append_url_constraint(c)
+        self.cm.append_url_constraint(d)
+        assert self.cm.url_constraints == [c, d]
+
+    def test_release(self):
+        i = self.cm.concurrent
+        self.cm.release()
+        assert self.cm.concurrent == i - 1
+
+    def test_acquire(self):
+        i = self.cm.concurrent
+        self.loop.run_until_complete(self.cm.acquire())
+        assert self.cm.concurrent == i + 1
+
+    def test_set_concurrent_crawlers(self):
+        i = self.cm.CONCURRENT_MAX
+        self.cm.set_concurrent_crawlers(i+1)
+        assert self.cm.CONCURRENT_MAX == i + 1
 
     def teardown_method(self, method):
         self.loop.stop()
